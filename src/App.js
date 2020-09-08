0ff6f0b3f4d7d75882e6617e97c3a3636e0099f5
Ferreis <rafa_ferreis@hotmail.com>
2020-09-08 17:21:58 -0300

    teste

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Vamos contar:</p>
+        <button>Contar</button>
+        <a
+          className="App-link"
+          href="http://pudim.com.br/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          pudim
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
